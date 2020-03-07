58c30363e8509d61e09851c008bb5467c9497ab9
alialkh <alialkhazaly@hotmail.com>
2020-03-07 09:33:43 -0700

    Updated

--- my_project/test.py
+++ my_project/test.py
@@ -12,39 +12,44 @@ arch = [
 #    to_input()
 
     # block 1
+    # 224x224
     to_Conv("conv11",'', '', offset="(0,0,0)", to="(0,0,0)", height=64, depth=64, width=1 ),
-    to_Conv("conv12", '224x224', '', offset="(0,0,0)", to="(conv11-east)", height=64, depth=64, width=1 ),
+    to_Conv("conv12", '', '', offset="(0,0,0)", to="(conv11-east)", height=64, depth=64, width=1 ),
     to_Pool("pool1", offset="(3,0,0)", to="(conv12-east)", height=32, depth=32, width=1),
 
     to_connection("conv12", "pool1"),    
 
     #block2
+    #112x112
     to_Conv("conv21", '', '', offset="(0,0,0)", to="(pool1-east)", height=32, depth=32, width=2 ),
-    to_Conv("conv22", '112x112', '', offset="(0,0,0)", to="(conv21-east)", height=32, depth=32, width=2 ),
+    to_Conv("conv22", '', '', offset="(0,0,0)", to="(conv21-east)", height=32, depth=32, width=2 ),
     to_Pool("pool2", offset="(2,0,0)", to="(conv22-east)", height=16, depth=16, width=1),
 
     to_connection("conv22", "pool2"),
 
     #block3
+    #64x64
     to_Conv("conv31", '', '', offset="(0,0,0)", to="(pool2-east)", height=16, depth=16, width=3 ),
     to_Conv("conv32", '', '', offset="(0,0,0)", to="(conv31-east)", height=16, depth=16, width=3 ),
-    to_Conv("conv33", '64x64', '', offset="(0,0,0)", to="(conv32-east)", height=16, depth=16, width=3 ),
+    to_Conv("conv33", '', '', offset="(0,0,0)", to="(conv32-east)", height=16, depth=16, width=3 ),
     to_Pool("pool3", offset="(2,0,0)", to="(conv33-east)",height=8, depth=8, width=1),
 
     to_connection("conv33", "pool3"),
 
     #block4
+    # 32x32
     to_Conv("conv41", '', '', offset="(0,0,0)", to="(pool3-east)", height=8, depth=8, width=4 ),
     to_Conv("conv42", '', '', offset="(0,0,0)", to="(conv41-east)", height=8, depth=8, width=4 ),
-    to_Conv("conv43", '32x32', '', offset="(0,0,0)", to="(conv42-east)", height=8, depth=8, width=4 ),
+    to_Conv("conv43", '', '', offset="(0,0,0)", to="(conv42-east)", height=8, depth=8, width=4 ),
     to_Pool("pool4", offset="(2,0,0)", to="(conv43-east)", height=4, depth=4, width=1),
 
     to_connection("conv43", "pool4"),
 
     #block5
+    # 16x16
     to_Conv("conv51", '', '', offset="(0,0,0)", to="(pool4-east)", height=4, depth=4, width=5 ),
     to_Conv("conv52", '', '', offset="(0,0,0)", to="(conv51-east)", height=4, depth=4, width=5 ),
-    to_Conv("conv53", '16x16', '', offset="(0,0,0)", to="(conv52-east)", height=4, depth=4, width=5 ),
+    to_Conv("conv53", '', '', offset="(0,0,0)", to="(conv52-east)", height=4, depth=4, width=5 ),
     to_Pool("pool5", offset="(2,0,0)", to="(conv53-east)", height=2, depth=2, width=1),
 
     to_connection("conv53", "pool5"),
@@ -54,14 +59,15 @@ arch = [
 
     
     #block6 upsample
-    to_UnPool("block6Up", caption="16x16",  offset="(2,0,0)", to="(conv6-east)", height=4,depth=4,width=6, opacity=1),
+    # 16x16
+    to_UnPool("block6Up", caption="",  offset="(2,0,0)", to="(conv6-east)", height=4,depth=4,width=6, opacity=1),
     
     to_connection("conv6","block6Up"),
 
     #block5 upsample
     to_UnPool("block5Up", caption="",  offset="(2,0,0)", to="(block6Up-east)", height=8,depth=8,width=5, opacity=1),
 
-    to_skip('pool4', 'block5Up', pos="2"),
+    # to_skip('pool4', 'block5Up', pos="2"),
 
     to_connection("block6Up","block5Up"),
 
